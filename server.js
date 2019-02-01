f609c24477c4d7826ec923e08b96495bfeba9d91
leofavre <contato@leofavre.com>
2019-01-31 22:32:48 -0200

    First commit

---
+++ server.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const hbs = require('hbs');
+
+const { static } = express;
+const app = express();
+const PORT = 3000;
+
+hbs.registerPartials(`${__dirname}/views/partials`);
+
+app.set('view engine', 'hbs');
+app.use(static(`${__dirname}/public`));
+
+app.get('/', (req, res) => {
+  res.render('home.hbs', {
+    pageTitle: 'Home',
+    welcomeMessage: 'Hello, there!',
+    currentYear: new Date().getFullYear()
+  });
+});
+
+app.get('/about', (req, res) => {
+  res.render('about.hbs', {
+    pageTitle: 'About',
+    currentYear: new Date().getFullYear()
+  });
+});
+
+app.get('/bad', (req, res) => {
+  res.send({
+    errorMessage: 'Too lazy to GET anything.'
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+});
